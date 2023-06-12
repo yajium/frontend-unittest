80c8c23604420a00f2e145fa13e05171c7aaa3f3
yajium <yajiuma720@gmail.com>
2023-06-12 22:58:32 +0900

    fix: 4-5

--- src/04/05/greet.test.ts
+++ src/04/05/greet.test.ts
@@ -31,6 +31,7 @@ test("モック関数は関数の中でも実行できる", () => {
 test("モック関数は実行時の引数を記録している", () => {
   const mockFn = jest.fn();
   function greet(message: string) {
+    // モック関数は実行時に"hello"という引数を持って実行されたことを記録する
     mockFn(message);
   }
   greet("hello");
@@ -40,5 +41,6 @@ test("モック関数は実行時の引数を記録している", () => {
 test("モック関数はテスト対象の引数として使用できる", () => {
   const mockFn = jest.fn();
   greet("Jiro", mockFn);
+  // 記録した実行時の引数の内訳を検証することでスパイとして利用できる
   expect(mockFn).toHaveBeenCalledWith("Hello! Jiro");
 });
