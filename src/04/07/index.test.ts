210d5ff0a883a865c2c16637f5fa826a8ff847e4
yajium <yajiuma720@gmail.com>
2023-06-14 17:56:31 +0900

    fix: 4-7

--- src/04/07/index.test.ts
+++ src/04/07/index.test.ts
@@ -1,15 +1,18 @@
 import { greetByTime } from ".";
 
 describe("greetByTime(", () => {
+  // テストが実行される前に偽のタイマーを使用するように指示
   beforeEach(() => {
     jest.useFakeTimers();
   });
 
+  // テスト終了後はタイマーを戻すように指示
   afterEach(() => {
     jest.useRealTimers();
   });
 
   test("朝は「おはよう」を返す", () => {
+    // 偽のタイマーで使用される現在システム時刻を設定
     jest.setSystemTime(new Date(2023, 4, 23, 8, 0, 0));
     expect(greetByTime()).toBe("おはよう");
   });
