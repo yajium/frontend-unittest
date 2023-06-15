b801f07107704abadfe083e7a0c4e6a9b216fc9b
yajium <yajiuma720@gmail.com>
2023-06-15 11:19:09 +0900

    fix: 5-4

--- src/05/04/ArticleList.test.tsx
+++ src/05/04/ArticleList.test.tsx
@@ -4,6 +4,7 @@ import { items } from "./fixture";
 
 test("タイトルの表示", () => {
   render(<ArticleList items={items} />);
+  // screen.getByRole("heading").toHaveTextContent("記事一覧");ともかける
   expect(screen.getByRole("heading", { name: "記事一覧" })).toBeInTheDocument();
 });
 
@@ -14,15 +15,17 @@ test("items の数だけ一覧表示される", () => {
 
 test("items の数だけ一覧表示される", () => {
   render(<ArticleList items={items} />);
-  const list = screen.getByRole("list");
+  const list = screen.getByRole("list"); // listはul要素のこと
   expect(list).toBeInTheDocument();
+  // within(lis)はscreenと同様な要素取得APIが含まれている
   expect(within(list).getAllByRole("listitem")).toHaveLength(3);
 });
 
 test("一覧アイテムが空のとき「投稿記事がありません」が表示される", () => {
   render(<ArticleList items={[]} />);
+  // 存在しないことをテストする場合はqueryByRoleを使う
   const list = screen.queryByRole("list");
-  expect(list).not.toBeInTheDocument();
+  expect(list).not.toBeInTheDocument(); // nullが返ってくるため、not.toBeInTheDocument()か↓のtoBeNull()を使う
   expect(list).toBeNull();
   expect(screen.getByText("投稿記事がありません")).toBeInTheDocument();
 });
